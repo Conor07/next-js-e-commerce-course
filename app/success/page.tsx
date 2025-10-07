db19472ca9adbdb00fc421c9da511387a158c301
Conor Talbot <conor@Conors-MacBook-Pro-2.local>
2025-10-07 14:55:35 +0100

    Completed course

---
+++ app/success/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useCartStore } from "@/store/cartStore";
+import Link from "next/link";
+import React from "react";
+
+const SuccessPage: React.FC<{}> = () => {
+  const { clearCart } = useCartStore();
+
+  React.useEffect(() => {
+    clearCart();
+  }, [clearCart]);
+
+  return (
+    <div className="container mx-auto px-4 py-8 text-center">
+      <h1 className="text-3xl font-bold mb-4">Payment Successful</h1>
+
+      <p className="mb-4">
+        Thank you for your purchase. Your order is being processed.
+      </p>
+
+      <Link href={"/products"} className="text-blue-600 hover:underline">
+        Continue Shopping
+      </Link>
+    </div>
+  );
+};
+
+export default SuccessPage;
